80e8d79700c8d98245bb6fa77a7922ace0e0c539
Alex Gurvich <agurvich@u.northwestern.edu>
2019-02-01 12:42:22 -0600

    added quest contributions

--- run_patch_scales.sh
+++ run_patch_scales.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-#SBATCH -o log_out.o%j       # output and error file name (%j expands to jobID)
-#SBATCH -N 1              # total number of mpi tasks requested
-#SBATCH -n 1              # total number of mpi tasks requested
-#SBATCH -p skx-normal     # queue (partition) -- normal, development, etc.
-#SBATCH -t 24:00:00        # run time (hh:mm:ss) - 24 hours
-#SBATCH --mail-user=agurvich@u.northwestern.edu
-#SBATCH -A TG-AST140023
-#SBATCH -J patches_m12m           # job name"
+#!/bin/bash 
+#MSUB -N alex_g_interactive_session
+#MSUB -q cosmoshimem
+#MSUB -A b1026
+#MSUB -l walltime=48:00:00 
+#MSUB -l nodes=1:ppn=24
+#MSUB -l partition=quest5
 
-fname='m12m_res7100'
-cd /home1/04210/tg835099/starformation/scripts
+fname='h113'
+cd /home/abg6257/starformation/scripts
 python makeFullHistory.py --snaplow=700 --savename=$fname --mps=4 --patch=1
 python makeFullHistory.py --snaplow=700 --savename=$fname --mps=4 --patch=.3
 python makeFullHistory.py --snaplow=700 --savename=$fname --mps=4 --patch=.1
