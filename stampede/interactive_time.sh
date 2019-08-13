80501e9db09136b35280cdc454b423fd2cc0b139
Alex Gurvich <agurvich@u.northwestern.edu>
2019-08-13 11:26:49 -0500

    track tmux conf

--- stampede/interactive_time.sh
+++ stampede/interactive_time.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 function interactive_time(){
-    squeue -u $USER -o "%.20j %.10L" | grep 'idv'
+    squeue -u $USER -o "%.20j %8R %.10L" | grep 'idv'
 }
 
+    #squeue -u $USER -o "%.20j %.10L" | grep 'idv'
+#alias showmyq="squeue -u $USER -o \"%.10i %.20j %.10L %5D %8R %2t\""
 #watch -n 1 'squeue -u $USER -o "%.20j %.10L" | grep idv' 
 echo `interactive_time`
--- stampede/run_patch_scales.sh
+++ stampede/run_patch_scales.sh
@@ -3,14 +3,14 @@
 #SBATCH -N 1              # total number of mpi tasks requested
 #SBATCH -n 1              # total number of mpi tasks requested
 #SBATCH -p skx-normal     # queue (partition) -- normal, development, etc.
-#SBATCH -t 48:00:00        # run time (hh:mm:ss) - 48 hours
+#SBATCH -t 24:00:00        # run time (hh:mm:ss) - 24 hours
 #SBATCH --mail-user=agurvich@u.northwestern.edu
 #SBATCH -A TG-AST140023
 #SBATCH -J m12i_scales           # job name
 
 fname='m12i_res7100'
 cd $HOME/starformation/scripts
-#python makeFullHistory.py --snaplow=700 --savename=$fname --mps=4 
+#python makeFullHistory.py --snaplow=700 --savename=$fname --mps=4
 #python makeFullHistory.py --snaplow=700 --savename=$fname --mps=4 --patch=1
 #python makeFullHistory.py --snaplow=700 --savename=$fname --mps=4 --patch=.3
 python makeFullHistory.py --snaplow=700 --savename=$fname --mps=1 --patch=.1
