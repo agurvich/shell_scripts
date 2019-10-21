6d9eb261604e593d17ca8cce28602a733c7303ad
Alex Gurvich <agurvich@u.northwestern.edu>
2019-10-21 15:53:11 -0500

    track quest scripts

---
+++ quest/interactive_time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+function interactive_time(){
+    squeue -u $USER -o "%.20j %8R %.10L" | grep 'bash'
+}
+
+#watch -n 1 'squeue -u $USER -o "%.20j %.10L" | grep idv' 
+echo -e you are doing a good job '\t' `interactive_time`
---
+++ quest/run_patch_scales.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -o %j_out.o       # output and error file name (%j expands to jobID)
+#SBATCH -N 1              # total number of nodes
+#SBATCH -n 1              # total number of mpi tasks requested
+#SBATCH -p cosmoscompute     # queue (partition) -- cosmoscompute or cosmoshimem
+#SBATCH -t 48:00:00        # run time (hh:mm:ss) - 4 hours
+#SBATCH --mail-user=agurvich@u.northwestern.edu
+#SBATCH -A b1026
+
+gal_str="m12i"
+fname="${gal_str}_res7100"
+
+
+snapnums=( 564 567 570 573 576 579 582 585 588 600)
+patches=( 3 1 0.3 0.1 )
+for snapnum in "${snapnums[@]}"
+do
+    for patch in "${patches[@]}"
+    do
+        cat preamble.sh > temp.sh 
+        echo "#SBATCH -J ${gal_str}_${snapnum}_${patch}           # job name" >> temp.sh
+        echo "cd $HOME/starformation/scripts" >> temp.sh
+        echo "export NUMBA_NUM_THREADS=24"
+        echo "python makeFullHistory.py --snaplow=$snapnum --snaphigh=$snapnum --savename=$fname --mps=1 --patch=${patch}" >> temp.sh
+        less temp.sh
+    done
+done
